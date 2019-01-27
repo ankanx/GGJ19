4c26e0a3d8fadd87980574011c0ebe1fa13d3488
Andreas Fransson <Ankanx@gmail.com>
2019-01-27 14:11:00 +0100

    update

--- GGJ19/Assets/Scripts/loadingimagerandom.cs
+++ GGJ19/Assets/Scripts/loadingimagerandom.cs
@@ -11,6 +11,11 @@ public class loadingimagerandom : MonoBehaviour
     Sprite poppysad;
     Sprite poppyhappy;
     Sprite poppyangry;
+    Sprite kidhappy;
+    Sprite kid;
+    Sprite kidsuprised;
+    Sprite oldlady;
+    Sprite oldlady2;
     List<Sprite> characterList = new List<Sprite>();
 
     
@@ -24,10 +29,20 @@ public class loadingimagerandom : MonoBehaviour
         poppyhappy = Resources.Load<Sprite>("Images/poppyhappy");
         poppysad = Resources.Load<Sprite>("Images/poppysad");
         poppyangry = Resources.Load<Sprite>("Images/poppyanger");
+        kid = Resources.Load<Sprite>("Images/kiddo");
+        kidhappy = Resources.Load<Sprite>("Images/kiddo!");
+        kidsuprised = Resources.Load<Sprite>("Images/kiddohappy");
+        oldlady = Resources.Load<Sprite>("Images/grandma");
+        oldlady2 = Resources.Load<Sprite>("Images/grandmahappy");
         characterList.Add(poppy);
         characterList.Add(poppysad);
         characterList.Add(poppyhappy);
         characterList.Add(poppyangry);
+        characterList.Add(kid);
+        characterList.Add(kidhappy);
+        characterList.Add(kidsuprised);
+        characterList.Add(oldlady);
+        characterList.Add(oldlady2);
         int ran = Random.Range(0, characterList.Count);
         Debug.Log(ran);
         GetComponent<Image>().sprite = characterList[ran];
